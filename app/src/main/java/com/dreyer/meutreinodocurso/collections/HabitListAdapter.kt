e490c97c1a0f8752ba1d957e2199acda1414fca0
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2023-07-18 17:30:53 -0300

    setting onBindViewHolder, asyncListDiffer and DiffCallback

--- app/src/main/java/com/dreyer/meutreinodocurso/collections/HabitListAdapter.kt
+++ app/src/main/java/com/dreyer/meutreinodocurso/collections/HabitListAdapter.kt
@@ -3,26 +3,29 @@ package com.dreyer.meutreinodocurso.collections
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import javax.net.ssl.SSLSessionBindingEvent
+
 
 /**
- * Descrever aqui essa primeira parte do adapter
+ * RecyclerView Adapter (*Adapter is a design pattern) for displaying a list of habits.
+ *
+ * The UI is based on the [HabitItemBinding].OBS=FAZER AINDA
+ *  We use the [HabitItem] as a model for the binding.
  */
 
 class HabitListAdapter : RecyclerView.Adapter<HabitListAdapter.ViewHolder>() {
 
-    //private val asyncListDiffer: AsyncListDiffer.......
+    private val asyncListDiffer: AsyncListDiffer<HabitItem> = AsyncListDiffer(this, DiffCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = HabitItemBinding.inflate(layoutInflater, parent, false)
         return ViewHolder(binding)
-
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(asyncListDiffer.currentList[position])
     }
 
     override fun getItemCount(): Int {
@@ -31,12 +34,22 @@ class HabitListAdapter : RecyclerView.Adapter<HabitListAdapter.ViewHolder>() {
 
     //create a new instance of ViewHolder that contains the layout xml of a list item
     //vai 'segurar' a view p depois ser inflada,ou seja, segura o layout xml depois so precisa mudar a info dentro
-    class ViewHolder(private val binding: HabitItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding: HabitItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(habit: HabitItem) {
             binding.titleTextView.text = habit.title
             binding.completeCheckBox.isChecked = habit.isCompleted
+        }
+    }
+
+    object DiffCallback : DiffUtil.ItemCallback<HabitItem>() {
+
+        override fun areItemsTheSame(oldItem: HabitItem, newItem: HabitItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: HabitItem, newItem: HabitItem): Boolean {
+            return oldItem == newItem
         }
     }
 
