11e722bae3bfb71883532d3f2d42ccfaa3096d80
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2023-08-08 17:45:39 -0300

    adding divider item decoration

--- app/src/main/java/com/dreyer/meutreinodocurso/collections/HabitListFragment.kt
+++ app/src/main/java/com/dreyer/meutreinodocurso/collections/HabitListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dreyer.meutreinodocurso.R
 import com.dreyer.meutreinodocurso.dummy.MockHabits
+import com.google.android.material.divider.MaterialDividerItemDecoration
 
 /**
  * A [Fragment] that displays a list of habits.
@@ -28,5 +31,25 @@ class HabitListFragment : Fragment () {
         binding.habitRecyclerView.adapter = adapter
         //updating the list of habits
         adapter.updateHabits(MockHabits.habitItemList)
+    }
+    private fun addingDividerDecoration(){
+        //Adding line between items with MaterialDividerItemDecoration
+        val divider = MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
+
+        //Removing the line at the end of the list
+        divider.isLastItemDecorated = false
+
+        //chamo resources aqui pra depois poder apenas referenciar abaixo
+        //nao colocar as dimensoes hardcoded
+        val resources = requireContext().resources
+
+        //Adding start spacing
+        divider.dividerInsetStart = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
+
+        //Defining size of the line
+        divider.dividerThickness = resources.getDimensionPixelSize(R.dimen.divider_height)
+        divider.dividerColor = ContextCompat.getColor(requireContext(), R.color.primary_200)
+
+        binding.habitRecyclerView.addItemDecoration(divider)
     }
 }
